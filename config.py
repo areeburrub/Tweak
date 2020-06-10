db5911e038120629e824bf5d765833228e580112
areeburrub <areeburrub@gmail.com>
2020-06-10 13:30:27 +0530

    config

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 #default congig
 class BaseConfig(object):
     DEBUG=False
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     TESTING = False
     SECRET_KEY = 'Thisissecret'
 
